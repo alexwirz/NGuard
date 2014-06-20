a975a93e993de012e50e13b7e808329d2db253a6
Alexander Wirz <alex.wirz@gmail.com>
2014-06-20 18:04:55 +0200

    simplest Requres.That() implementation

---
+++ src/NGuard/NGuard.Specs/RequiresSpecs.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGuard.Specs
+{
+    [TestFixture]
+    public class RequiresSpecs
+    {
+        [Test]
+        [ExpectedException(typeof(ContractViolated))]
+        public void WhenConditionIsFalseThatThrowsException()
+        {
+            Requires.That(1 > 2);
+        }
+
+        [Test]
+        public void WhenConditionIsTrueNoExceptionIsThrown()
+        {
+            Requires.That(2 * 2 == 4);
+        }
+    }
+}
---
+++ src/NGuard/NGuard/Requires.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGuard
+{
+    public class Requires
+    {
+        public static void That(bool condition)
+        {
+            if (!condition) throw new ContractViolated();
+        }
+    }
+}
